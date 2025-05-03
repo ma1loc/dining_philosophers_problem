a2e85a8e7f8cc015f1959fb9505ad5966d5c00d7
Younes Anflous <anayouness51@gmail.com>
2025-05-03 18:47:38 +0100

    philo function update

--- srcs/_philo.c
+++ srcs/_philo.c
@@ -1,33 +1,10 @@
 # include "_philo.h"
 
-void	setup_user_args(t_setup *setup, char **argv)
-{
-	setup->user_args = gc_malloc(setup->gc, sizeof(t_user_args));
-	if (!setup->user_args)
-		cleanup_and_exit(setup, "[-] Error: malloc failed.\n");	// use cleanup_and_exit
-	setup->user_args->nbr_of_philo = ft_atoi(argv[1]);
-	setup->user_args->time_to_die = ft_atoi(argv[2]);
-	setup->user_args->time_to_eat = ft_atoi(argv[3]);
-	setup->user_args->time_to_sleep = ft_atoi(argv[4]);
-	setup->user_args->max_meals = -1;
-	if (argv[5])
-		setup->user_args->max_meals = ft_atoi(argv[5]);
-}	
+// Each philosopher should be a thread.
 
-t_setup	*init_setup(int argc, char **argv)
+void	singal_philo(t_setup *setup)
 {
-	t_setup	*setup;
-	t_gc	*gc;
-
-	gc = gc_init();
-	setup = gc_malloc(gc, sizeof(t_setup));
-	if (!setup)
-		return (cleanup_and_exit(setup, "[-] Error: malloc failed.\n"), NULL);
-	// memset(setup, 0, sizeof(t_setup));
-	setup->gc = gc;
-	if (input_parsing(argc, argv))
-		return (gc_destroy(setup->gc), NULL);
-	return (setup);
+	// what is have to do when i have one philo ???
 }
 
 int	main(int argc, char **argv)
@@ -38,5 +15,11 @@ int	main(int argc, char **argv)
 	if (!setup)
 		return (1);
 	setup_user_args(setup, argv);
-	
+	// >>>> here >>>>
+	if (setup->user_args->nbr_of_philo == 1)	// case of one philo.
+		singal_philo(setup);
+	else
+		multiple_philo(setup);
+	// >>>>>>>>>>>>>>
+	gc_destroy(setup->gc);
 }
