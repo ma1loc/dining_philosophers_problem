9821e3cb4c34632bddaf4b3d4a9af6de5f61739d
Younes Anflous <anayouness51@gmail.com>
2025-05-03 18:47:50 +0100

    Header file update

--- srcs/_philo.h
+++ srcs/_philo.h
@@ -12,19 +12,21 @@ typedef struct s_mem
 	void			*ptr;
 	struct s_mem	*next;
 }	t_mem;
+
 typedef struct	s_gc
 {
 	t_mem			*mem_list;
 	int				total_allocs;
 	size_t			total_bytes;
 }	t_gc;
+
 typedef struct	s_user_args
 {
 	int	nbr_of_philo;
 	int	time_to_die;
 	int	time_to_eat;
 	int	time_to_sleep;
-	int	max_meals;
+	int	must_eat_meals;
 }	t_user_args;
 
 typedef struct s_setup
@@ -33,15 +35,17 @@ typedef struct s_setup
 	t_user_args	*user_args;
 } t_setup;
 
-int		ft_atoi(char *str);
+int		ft_atoi(char *str, t_setup *setup);
 size_t	ft_strlen(char *str);
 void	ft_putstr_fd(char *s, int fd);
+t_setup	*init_setup(int argc, char **argv);
+void	setup_user_args(t_setup *setup, char **argv);
 t_gc	*gc_init(void);
 void	*gc_malloc(t_gc *gc, size_t size);
 void	gc_free(t_gc *gc, void *ptr);
 void	gc_destroy(t_gc *gc);
-int		input_parsing(int argc, char **argv);
+void	input_parsing(int argc, char **argv, t_setup *setup);
 void	cleanup_and_exit(t_setup *setup, char *msg);
-// void	cleanup_and_exit();
+
 
 # endif
